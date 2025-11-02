3dbfe0a98d091da6e37e74a4564b1567b19e36ca
Lilian <210286008+liliann19@users.noreply.github.com>
2025-11-02 06:40:37 -0800

    Created tests for maxDiff

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -3,8 +3,6 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class PracticeTest {
-
-    // TODO: Make more tests for maxDiff
     @Test
     void testMaxDiffPositiveAndNegative() {
         // Arrange
@@ -17,6 +15,22 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
+    @Test
+    void testMaxDiffOneNum() {
+        int[] numbers = {8};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(0, actual);
+    }
+    @Test
+    void testMaxDiffAllNegative() {
+        int[] numbers = {-8, -2, -9, -5};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(7, actual);
+    }
+    
+
+
+
     
 
     // TODO: Make tests for each problem you solve
